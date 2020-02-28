17146744721bb7880e2addb8d276bc1ac6fd8027
lstacey512 <poppies.in.pink@gmail.com>
2020-02-27 19:02:22 -0600

    adding final files for demo!

--- Met_website/flask_app.py
+++ Met_website/flask_app.py
@@ -15,7 +15,7 @@ import sys
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Met_test_db.db")
+engine = create_engine("sqlite:///Met_final_db.db")
 
 # reflect an existing database into a new model
 Base = declarative_base()
@@ -30,7 +30,7 @@ class Paintings(Base):
     Medium = Column(String)
     Met_link = Column(String)
     Artist_id = Column(Integer, ForeignKey('Artists.Artist_id'))
-    Dept_id = Column(Integer)
+    Dept_id = Column(Integer, ForeignKey('Departments.Dept_id'))
     painting_colors = relationship("Paintings_colors", backref="Paintings")
 
 class Paintings_colors(Base):
@@ -59,6 +59,11 @@ class Artists(Base):
     Image_link = Column(String)
     paintings = relationship("Paintings", backref="Artists")
 
+class Departments(Base):
+    __tablename__ = 'Departments'
+    Dept_name = Column(String)
+    Dept_id = Column (Integer, primary_key = True)
+    paintings = relationship("Paintings", backref="Departments")
 
 #################################################
 # Flask Setup
@@ -100,7 +105,7 @@ def search():
     #return the painting id, title, date, image link and artist name, filtered where color is >= 30 from Paintings_colors
     results_q = session.query(Paintings.Object_id, Paintings.Title, Paintings.Date_finish, Paintings.Met_link, Artists.Full_name, func.sum(Paintings_colors.Size)).\
         join(Artists, Paintings.Artist_id == Artists.Artist_id).join(Paintings_colors, Paintings_colors.Object_id == Paintings.Object_id).\
-            join(Colors, Paintings_colors.Color_name == Colors.Color_name).filter(Colors.Group == selected_color).group_by(Paintings.Object_id).having(func.sum(Paintings_colors.Size) > 30)
+            join(Colors, Paintings_colors.Color_name == Colors.Color_name).filter(Colors.Group == selected_color).group_by(Paintings.Object_id).having(func.sum(Paintings_colors.Size) > 20)
     
     results = results_q.order_by(func.sum(Paintings_colors.Size).desc()).all()
     
@@ -132,6 +137,8 @@ def painting():
     if Selected_painting == "None":
         return render_template("404.html", error = "You want to see a painting, but haven't told us which one you want to see yet! Try searching the collection or selecting a painting in the artist's section.")
     
+
+
     else: 
         #start session and query
         session = Session(engine)
@@ -174,16 +181,17 @@ def artist():
 
     #get the selected painting ID
     Artist_id = request.args.get('id')
-    Artist_name = request.form['artist']
+    #Artist_name = request.form['artist']
 
     print(Artist_id, file=sys.stderr)
-    print(Artist_name, file=sys.stderr)
+    #print(Artist_name, file=sys.stderr)
 
-    if Artist_name != "None":
-        session = Session(engine)
-        Artist_id = session.query(Artists.Artist_id).filter(Artists.Full_name.like('%'+Artist_name+'%')).one()
-        print(Artist_id, file=sys.stderr)
-        session.close()
+    # if Artist_name != "None":
+    #     session = Session(engine)
+    #     id_Lookup = session.query(Artists.Artist_id).filter(Artists.Full_name.like('%'+Artist_name+'%')).one()
+    #     Artist_id = id_Lookup[0]
+    #     print(Artist_id, file=sys.stderr)
+    #     session.close()
 
     #if we have an id arguement and it's not assocaited with an "unknown author"
     if Artist_id == 2 or Artist_id == 320 or Artist_id == 684:
@@ -236,54 +244,144 @@ def artist():
 
         return render_template('aboutArtist.html', artist_results = artist_data, painting_results = painting_data, painting_refer = Painting_referral)
 
+@app.route("/SearchArtist", methods=["GET", "POST"])
+def SearchArtist():
+
+    #get the selected painting ID
+    Artist_id = request.args.get('id')
+    Artist_name = request.form['artist']
+
+    print(Artist_id, file=sys.stderr)
+    print(Artist_name, file=sys.stderr)
+
+    if Artist_name != "None":
+        session = Session(engine)
+        try: 
+            id_Lookup = session.query(Artists.Artist_id).filter(Artists.Full_name.like('%'+Artist_name+'%')).one()
+            Artist_id = id_Lookup[0]
+            print(Artist_id, file=sys.stderr)
+        except:
+            Artist_id = 2
+        session.close()
+
+    #if we have an id arguement and it's not assocaited with an "unknown author"
+    if Artist_id == 2 or Artist_id == 320 or Artist_id == 684:
+        return render_template ("404.html", error = "We couldn't seem to find the artist. They might be unknown or don't exist in the Met's collection. Try again by refining your search on the homepage or by selecting a painting via search.")
+
+    else:
+        #if we have an id, that means we've been referred by a painting result. Grab the refering painting id to enable the toggler. 
+        Painting_referral = request.args.get('referral')
+        #start session and query
+        session = Session(engine)
+
+        #return the artist's information 
+        results = session.query(Artists.Full_name, Artists.Birth_date, Artists.Death_date, Artists.Biography, Artists.Image_link).\
+            filter(Artists.Artist_id == Artist_id).all()
+   
+        paintings = session.query(Paintings.Object_id, Paintings.Met_link, Paintings.Title).\
+            join(Artists, Paintings.Artist_id == Artists.Artist_id ).filter(Artists.Artist_id == Artist_id).all()
+        
+        artist_collection = []
+        for Object_id, Met_link, Title in paintings:
+            painting_dict = {}
+            painting_dict["Object_id"] = Object_id
+            painting_dict["Image"] = Met_link
+            painting_dict["Title"] = Title
+
+            color_results = session.query(Paintings_colors.Hex).filter(Paintings_colors.Object_id == Object_id).all()
+            all_colors = list(np.ravel(color_results))
+
+            painting_dict["Colors"] = all_colors
+
+            artist_collection.append(painting_dict)
+        
+        session.close()
+
+        artist_info = []
+        for Full_name, Birth_date, Death_date, Biography, Image_link in results:
+            result_dict = {}
+            result_dict["Artist_Name"] = Full_name
+            result_dict["Birth_date"] = Birth_date
+            result_dict["Death_date"] = Death_date
+            result_dict["Biography"] = Biography
+            result_dict["Image"] = Image_link
+            artist_info.append(result_dict)
+        
+        artist_data = json.dumps(artist_info)
+        print(artist_data,file=sys.stderr)
+
+        painting_data = json.dumps(artist_collection)
+        #print(painting_data,file=sys.stderr)
+
+        return render_template('aboutArtist.html', artist_results = artist_data, painting_results = painting_data, painting_refer = Painting_referral)
+
+
 @app.route("/overview")
 def overview():
 
     session = Session(engine)
 
-    paintings_summary = session.query(Paintings.Object_id, Paintings.Dept_id, Paintings.Medium, Artists.Full_name).\
-        join(Artists, Paintings.Artist_id == Artists.Artist_id).all()
+    all_hex = session.query(Paintings_colors.Hex.distinct()).group_by(Paintings_colors.Hex).join(Colors, Paintings_colors.Color_name == Colors.Color_name).\
+        filter(Colors.Group != "Black").having(func.sum(Paintings_colors.Size) > 20).order_by(Colors.Group).all()
 
     colors_summary = session.query(Paintings_colors.Color_name, func.sum(Paintings_colors.Size)).group_by(Paintings_colors.Color_name).all()
 
-    color_groups = session.query(Colors.Group, func.sum(Paintings_colors)).join (Paintings_colors, Colors.Color_name == Paintings_colors.Color_name).\
-        group_by(Colors.Group).all()
+    mediums = session.query(Paintings.Medium, func.count(Paintings.Medium)).group_by(Paintings.Medium).\
+        having(func.count(Paintings.Medium) > 10).all()
+
+    dept_q = session.query(Departments.Dept_name, func.count(Paintings.Dept_id)).join(Paintings, Departments.Dept_id == Paintings.Dept_id).\
+        group_by(Paintings.Dept_id).all()
+
+    artist_results = session.query (Artists.Full_name, func.count(Paintings.Artist_id)).join(Paintings, Artists.Artist_id == Paintings.Artist_id).\
+        group_by(Artists.Full_name).order_by(func.count(Paintings.Artist_id).desc()).limit(6).all()
+
+    # color_groups = session.query(Colors.Group, func.sum(Paintings_colors.Size)).join(Paintings_colors, Colors.Color_name == Paintings_colors.Color_name).\
+    #     group_by(Colors.Group).all()
 
     session.close()
-
-    collection = []
-    for Object_id, Dept_id, Medium, Full_name in paintings_summary:
-        result_dict = {}
-        result_dict["Object_id"] = Object_id
-        result_dict["Dept_id"] = Dept_id
-        result_dict["Medium"] = Medium
-        result_dict["Artist_Name"] = Full_name
-        collection.append(result_dict)
-
-    main_data = json.dumps(collection)
-    #print(main_data,file=sys.stderr)
+    
+    hex_list = [{"Hex": list(np.ravel(all_hex))}]
+    hex_colors = json.dumps(hex_list)
 
     colors_sub = []
     for Color_name, Size in colors_summary:
         sub_dict = {}
         sub_dict["Color_name"] = Color_name
         sub_dict["Size"] = Size
-        color_sub.append(sub_dict)
+        colors_sub.append(sub_dict)
 
-    sub_color_data = json.dumps(color_sub)
+    sub_color_data = json.dumps(colors_sub)
     #print(sub_color_data,file=sys.stderr)
 
-    colors_group = []
-    for Group, Size in colors_summary:
-        group_dict = {}
-        group_dict["Color_group"] = Group
-        group_dict["Size"] = Size
-        color_group.append(group_dict)
+    painting_mediums = []
+    for Medium, Count in mediums:
+        med_dict = {}
+        med_dict["Medium"] = Medium
+        med_dict["Count"] = Count
+        painting_mediums.append(med_dict)
+    
+    top_mediums = json.dumps(painting_mediums)
+    #print(painting_mediums, file=sys.stderr)
 
-    group_color_data = json.dumps(color_group)
-    #print(group_color_data,file=sys.stderr)
+    painting_departments = []
+    for Department, Count in dept_q:
+        dept_dict = {}
+        dept_dict["Department"] = Department
+        dept_dict["Count"] = Count
+        painting_departments.append(dept_dict)
+    
+    department_summary = json.dumps(painting_departments)
 
-    return render_template('overview.html', met_summary = main_data, sub_colors = sub_color_data, group_color = group_color_data )
+    artist_summary = []
+    for Full_name, Count in artist_results:
+        artist_dict = {}
+        artist_dict["Full_name"] = Full_name
+        artist_dict["Count"] = Count
+        artist_summary.append(artist_dict)
+    
+    top_artists = json.dumps(artist_summary)
+
+    return render_template('overview.html', hex_colors = hex_colors, group_colors = sub_color_data, top_mediums = top_mediums, department_summary = department_summary, top_artists = top_artists)
         
 if __name__ == "__main__":
     app.run(debug=True)
